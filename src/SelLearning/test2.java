f082bfdda83e8175dfdb81a7e27462e5afed0c80
nhs <nethra.hs@infor.com>
2023-07-21 15:12:20 +0530

    first commit

---
+++ src/SelLearning/test2.java
@@ -0,0 +1,17 @@
+package SelLearning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test2 {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty( "webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        // Instantiate a ChromeDriver class.
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://infornexus-rctp.qa.gtnexus.com/");
+
+	}
+
+}
